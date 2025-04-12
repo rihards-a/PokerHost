7a6db77fbde44be675ea9e35da150186fa6ae0e4
rih <rih@rih.rih>
2025-04-12 15:41:42 +0300

    Created the backbone migrations for the game and transactions

--- database/migrations/2025_04_12_115542_create_transactions_table.php
+++ database/migrations/2025_04_12_115542_create_transactions_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('hand_id')->constrained()->onDelete('cascade');
+            $table->foreignId('hand_player_id')->nullable()->constrained()->nullOnDelete();
+            $table->integer('amount');
+            $table->enum('type', ['bet', 'win', 'buyin', 'refund']);
             $table->timestamps();
         });
     }
