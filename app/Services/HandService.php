b72375643a077136673ea06bc0bbdc9a3192afe3
Rihards <rihards.abolins1@gmail.com>
2025-05-06 16:21:13 +0300

    Updated database model creation to follow best practices

--- app/Services/HandService.php
+++ app/Services/HandService.php
@@ -9,6 +9,14 @@ use App\Models\Table;
 
 class HandService
 {
+    protected $deckService, $positionService;
+
+    public function __construct(DeckService $deckService, PositionService $positionService)
+    {
+        $this->deckService = $deckService;
+        $this->positionService = $positionService;
+    }
+
     /*
     * Initialize a new hand for the table.
     *
@@ -24,22 +32,24 @@ class HandService
         */
 
         // Deal cards to the table. Determine dealer, small blind, and big blind
-        $hand = new Hand();
-        $hand->table_id = $table->id;
-        $hand->community_cards = ["Ah", "Kd", "Qs", "Jc", "9h"]; #TODO create a deck and shuffle - make sure users cannot see this or deal it incrementally
-        $hand->dealer_id = $occupiedSeats->first()->id; #TODO maybe use a dealer offset and then skip 'offset % count'
-        $hand->small_blind_id = $occupiedSeats->find($hand->dealer_id)->nextActive->id;
-        $hand->big_blind_id = $occupiedSeats->find($hand->small_blind_id)->nextActive->id; #TODO edge case if only 2 players
-        $hand->save();
+        $hand = Hand::create([
+            'table_id' => $table->id,
+            'community_cards' => ["Ah", "Kd", "Qs", "Jc", "9h"], #TODO create a deck and shuffle - make sure users cannot see this or deal it incrementally
+            'dealer_id' => $occupiedSeats->first()->id, #TODO maybe use a dealer offset and then skip 'offset % count'
+            'small_blind_id' => $occupiedSeats->find($occupiedSeats->first()->id)->nextActive->id,
+            'big_blind_id' => $occupiedSeats->find($occupiedSeats->first()->id)->nextActive->nextActive->id, #TODO edge case if only 2 players
+            'is_complete' => false,
+        ]);
 
         // Deal cards to each player
         foreach ($occupiedSeats as $seat) {
-            $seatHand = new SeatHand();
-            $seatHand->hand_id = $hand->id;
-            $seatHand->seat_id = $seat->id;
-            $seatHand->card1 = "As"; #TODO create a deck and shuffle
-            $seatHand->card2 = "Ad"; #TODO create a deck and shuffle
-            $seatHand->save();
+            SeatHand::create([
+                'status' => 'active',
+                'hand_id' => $hand->id,
+                'seat_id' => $seat->id,
+                'card1' => 'As', #TODO create a deck and shuffle - make sure users cannot see this or deal it incrementally
+                'card2' => 'Ks', #TODO create a deck and shuffle - make sure users cannot see this or deal it incrementally
+            ]);
         }
 
         return $hand;
@@ -49,5 +59,9 @@ class HandService
     {
         // TODO maybe calculate winners, maybe this is unnecessary in this service
         // maybe deal out all the cards and then calculate winners if all-in situation, maybe in different service...
+        $winners = [];
+        $seatHands = $hand->seatHands()->where('folded', false)->get();
+
+        return $winners;
     }
 }
