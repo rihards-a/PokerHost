9af93c06d7615e65e0323489e38b6fd90a415153
Rihards <rihards.abolins1@gmail.com>
2025-05-16 18:52:58 +0300

    Service now updates the loser and winner hand statuses - won or busted, and has better loading of necessary models.

--- app/Services/HandService.php
+++ app/Services/HandService.php
@@ -65,17 +65,19 @@ class HandService
         }
         // TODO deal out all the cards - configure the round service to do this one by one - or rather call its methods one by one since no players are 'active' yet not 'folded'
         $winners = [];
-        $seatHands = $hand->seatHands()->whereNot('status', 'folded')->with('seat')->get();
+        $seatHands = $hand->seatHands()->whereNot('status', 'folded')->with('seat.player')->get();
 
         $dealerSeat = $hand->table->seats()->find($hand->dealer_id);
         $totalSeats = $hand->table->seats()->count(); // Needed for wraparound
 
         foreach ($seatHands as $seatHand) {
-            $handRank = $this->deckService->evaluateHand($seatHand->cards, $hand->community_cards);
+            $handRank = $this->deckService->evaluateHand([$seatHand->card1, $seatHand->card2], json_decode($hand->community_cards));
             $winners[] = [
                 'seat_id' => $seatHand->seat_id,
                 'hand_rank' => $handRank,
                 'distance_from_dealer' => ($seatHand->seat->position - $dealerSeat->position + $totalSeats) % $totalSeats,
+                'seat_hand' => $seatHand, // Include the entire seatHand object
+                'player' => $seatHand->seat->player, // Include the player object
             ];
         }
 
@@ -83,11 +85,33 @@ class HandService
 
         // Distribute the pot to the winners
         $best_hand = $winners[0]['hand_rank'];
+        $everyone = $winners;
         $winners = array_filter($winners, function ($winner) use ($best_hand) {
             return $winner['hand_rank'] === $best_hand;
         });
         $winners = collect($winners)->sortBy('distance_from_dealer')->values()->all();
         $this->transactionService->distributePot($hand->pot_size, $winners);
+
+        // Update database status for seatHands
+        $winner_ids = array_column($winners, 'seat_id');
+        $losers = array_filter($everyone, function ($player) use ($winner_ids) {
+            return !in_array($player['seat_id'], $winner_ids);
+        });
+        $losers = array_values($losers); // Re-index the array to account for missing values
+
+        // Update seatHand status - win or bust
+        foreach ($winners as $winner) {
+            $winner['seat_hand']->update([
+                'status' => 'won',
+            ]);
+        }
+        foreach ($losers as $loser) {
+            $loser['seat_hand']->update([
+                'status' => 'busted',
+            ]);
+        }
+
+        #TODO show the cards of players who won
 
         return $winners;
     }
