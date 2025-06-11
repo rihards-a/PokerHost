dbe6c7d9ba7ad28cee846d1e2727b0cb4d1509b5
Frederiks Kronbergs <theguitarcuber16@gmail.com>
2025-06-11 16:43:10 +0300

    Small update - added navbar to table view

--- resources/js/Pages/Tables/Show.vue
+++ resources/js/Pages/Tables/Show.vue
@@ -1,4 +1,5 @@
 <template>
+  <AppLayout>
     <div class="container mx-auto p-4">
       <h1 class="text-2xl font-bold mb-4">Poker Table: {{ table.name }}</h1>
       
@@ -202,13 +203,19 @@
         </div>
       </div>
     </div>
+    </AppLayout>
   </template>
   
   <script>
   import { defineComponent } from 'vue';
   import Pusher from 'pusher-js';
+  import AppLayout from '@/Layouts/AppLayout.vue';
   
   export default defineComponent({
+    components: {
+      AppLayout,
+
+    },
     props: {
       table: Object,
       seats: Array,
